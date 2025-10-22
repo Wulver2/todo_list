c3e242ba57acde8d206e421ffe811d01d2c65fd2
Wulver2 <ross.shayla21@gmail.com>
2025-10-22 09:48:51 -0400

    Created constructor for items that will make up the todo lists

---
+++ src/items.js
@@ -0,0 +1,12 @@
+//items object will be a class containing 
+
+export class Items {
+    constructor(title, description, dueDate, priority, notes, isDone) {
+        this.title = title; // string
+        this.description = description; // string
+        this.dueDate = dueDate;
+        this.priority = priority; // integer
+        this.notes = notes; // string
+        this.isDone = isDone // boolean value
+    }
+}
